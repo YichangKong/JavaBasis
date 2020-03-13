f644edeea029a1840dd482b0866feb527d9ef86f
YichangKong <50892995+YichangKong@users.noreply.github.com>
2020-03-13 16:44:47 +0800

    链表模拟栈作业完成 添加大黄 This reverts commit 6e36b1b8

--- src/DataStructures/Stack/LinkleStackDemo.java
+++ src/DataStructures/Stack/LinkleStackDemo.java
@@ -9,6 +9,43 @@ public class LinkleStackDemo {
 
     public static void main(String[] args) {
 
+        StringBuffer sb = new StringBuffer();
+        sb.append("                       ::\n");
+        sb.append("                      :;J7, :,                        ::;7:\n");
+        sb.append("                      ,ivYi, ,                       ;LLLFS:\n");
+        sb.append("                      :iv7Yi                       :7ri;j5PL\n");
+        sb.append("                     ,:ivYLvr                    ,ivrrirrY2X,\n");
+        sb.append("                     :;r@Wwz.7r:                :ivu@kexianli.\n");
+        sb.append("                    :iL7::,:::iiirii:ii;::::,,irvF7rvvLujL7ur\n");
+        sb.append("                   ri::,:,::i:iiiiiii:i:irrv177JX7rYXqZEkvv17\n");
+        sb.append("                ;i:, , ::::iirrririi:i:::iiir2XXvii;L8OGJr71i\n");
+        sb.append("              :,, ,,:   ,::ir@mingyi.irii:i:::j1jri7ZBOS7ivv,\n");
+        sb.append("                 ,::,    ::rv77iiiriii:iii:i::,rvLq@huhao.Li\n");
+        sb.append("             ,,      ,, ,:ir7ir::,:::i;ir:::i:i::rSGGYri712:\n");
+        sb.append("           :::  ,v7r:: ::rrv77:, ,, ,:i7rrii:::::, ir7ri7Lri\n");
+        sb.append("          ,     2OBBOi,iiir;r::        ,irriiii::,, ,iv7Luur:\n");
+        sb.append("        ,,     i78MBBi,:,:::,:,  :7FSL: ,iriii:::i::,,:rLqXv::\n");
+        sb.append("        :      iuMMP: :,:::,:ii;2GY7OBB0viiii:i:iii:i:::iJqL;::\n");
+        sb.append("       ,     ::::i   ,,,,, ::LuBBu BBBBBErii:i:i:i:i:i:i:r77ii\n");
+        sb.append("      ,       :       , ,,:::rruBZ1MBBqi, :,,,:::,::::::iiriri:\n");
+        sb.append("     ,               ,,,,::::i:  @arqiao.       ,:,, ,:::ii;i7:\n");
+        sb.append("    :,       rjujLYLi   ,,:::::,:::::::::,,   ,:i,:,,,,,::i:iii\n");
+        sb.append("    ::      BBBBBBBBB0,    ,,::: , ,:::::: ,      ,,,, ,,:::::::\n");
+        sb.append("    i,  ,  ,8BMMBBBBBBi     ,,:,,     ,,, , ,   , , , :,::ii::i::\n");
+        sb.append("    :      iZMOMOMBBM2::::::::::,,,,     ,,,,,,:,,,::::i:irr:i:::,\n");
+        sb.append("    i   ,,:;u0MBMOG1L:::i::::::  ,,,::,   ,,, ::::::i:i:iirii:i:i:\n");
+        sb.append("    :    ,iuUuuXUkFu7i:iii:i:::, :,:,: ::::::::i:i:::::iirr7iiri::\n");
+        sb.append("    :     :rk@Yizero.i:::::, ,:ii:::::::i:::::i::,::::iirrriiiri::,\n");
+        sb.append("     :      5BMBBBBBBSr:,::rv2kuii:::iii::,:i:,, , ,,:,:i@petermu.,\n");
+        sb.append("          , :r50EZ8MBBBBGOBBBZP7::::i::,:::::,: :,:,::i;rrririiii::\n");
+        sb.append("              :jujYY7LS0ujJL7r::,::i::,::::::::::::::iirirrrrrrr:ii:\n");
+        sb.append("           ,:  :@kevensun.:,:,,,::::i:i:::::,,::::::iir;ii;7v77;ii;i,\n");
+        sb.append("           ,,,     ,,:,::::::i:iiiii:i::::,, ::::iiiir@xingjief.r;7:i,\n");
+        sb.append("        , , ,,,:,,::::::::iiiiiiiiii:,:,:::::::::iiir;ri7vL77rrirri::\n");
+        sb.append("         :,, , ::::::::i:::i:::i:i::,,,,,:,::i:i:::iir;@Secbone.ii:::\n");
+        sb.append("\t\t阿黄保佑                                                  永无BUG\n");
+        System.out.println(sb.toString());
+
         LinkedListInt listInt = new LinkedListInt(5);
         listInt.add(1);
         listInt.add(2);
@@ -20,8 +57,20 @@ public class LinkleStackDemo {
         System.out.println(listInt.pop());
         System.out.println(listInt.pop());
         System.out.println(listInt.pop());
+        System.out.println("_________");
 
         listInt.list();
+
+        listInt.add(3);
+        listInt.add(4);
+        listInt.add(5);
+
+        System.out.println("__________");
+
+        listInt.list();
+        System.out.println("__________");
+
+        System.out.println(listInt.pop());
 
 
     }
